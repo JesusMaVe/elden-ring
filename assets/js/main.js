b4a7c0db9fab91599767d9d57ca3ffbc105dead6
Jesus Marentes <marentes13@outlook.com>
2025-03-07 01:15:39 -0600

    feat: remove unused images and add new stylesheets for DLC and NPC pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,21 @@
-const resizeBtn = document.getElementById("resize");
-resizeBtn.addEventListener("click", function (e) {
-  document.body.classList.toggle("sb-expand");
+const logoImg = document.getElementById("logo-img");
+const logoDialog = document.getElementById("logo-dialog");
+
+logoImg.addEventListener("click", (event) => {
+    event.stopPropagation(); 
+
+    if (logoDialog.open) {
+        logoDialog.classList.remove("show");
+        setTimeout(() => logoDialog.close(), 300); 
+    } else {
+        logoDialog.show();
+        setTimeout(() => logoDialog.classList.add("show"), 10); 
+    }
+});
+
+document.addEventListener("click", (event) => {
+    if (!logoDialog.contains(event.target) && event.target !== logoImg) {
+        logoDialog.classList.remove("show");
+        setTimeout(() => logoDialog.close(), 300);
+    }
 });
